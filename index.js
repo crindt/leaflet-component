62813742baab90f3a6290b5dcb70c7c38ad50edd
Craig Rindt <crindt@gmail.com>
2013-12-02 09:42:11 -0800

    Fix image path hack (again)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = require('./leaflet.js')
 // fix up path to installed images
-if ( module.exports ) 
+if ( module.exports.Icon ) 
   module.exports.Icon.Default.imagePath='crindt-leaflet-component/images'
-else if ( L )
+else if ( L.Icon )
   L.Icon.Default.imagePath='crindt-leaflet-component/images'
+else
+  console.log("Error fixing leaflet image path")
